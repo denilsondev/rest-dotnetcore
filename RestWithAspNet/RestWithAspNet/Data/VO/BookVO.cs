ff475e55edc4e27ddd3aae6875ec512bca79eb5b
denilsondev <denilsoncdc10@gmail.com>
2020-07-13 21:59:39 -0300

    feat: implementg data contracts

--- RestWithAspNet/RestWithAspNet/Data/VO/BookVO.cs
+++ RestWithAspNet/RestWithAspNet/Data/VO/BookVO.cs
@@ -1,14 +1,25 @@
 ﻿using RestWithAspNet.Model.Base;
 using System;
+using System.Runtime.Serialization;
 
 namespace RestWithAspNet.Data.VO
 {
+    [DataContract]
     public class BookVO
     {
+        [DataMember (Order = 1, Name = "id")]
         public long Id { get; set; }
+
+        [DataMember(Order = 2, Name = "title")]
         public string Title { get; set; }
+
+        [DataMember(Order = 3, Name = "name")]
         public string Author { get; set; }
+
+        [DataMember(Order = 4, Name = "price")]
         public decimal Price { get; set; }
+
+        [DataMember(Order = 5, Name = "laucnData")]
         public DateTime LaunchDate { get; set; }
     }
 }
--- RestWithAspNet/RestWithAspNet/Data/VO/PersonVO.cs
+++ RestWithAspNet/RestWithAspNet/Data/VO/PersonVO.cs
@@ -2,16 +2,27 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Threading.Tasks;
 
 namespace RestWithAspNet.Data.VO
 {
+    [DataContract]
     public class PersonVO
     {
+        [DataMember (Order = 1, Name = "id")]
         public long Id { get; set; }
+
+        [DataMember(Order = 2, Name = "lastName")]
         public string FirstName { get; set; }
+
+        [DataMember(Order = 3, Name = "order")]
         public string LastName { get; set; }
+
+        [DataMember(Order = 4, Name = "codigo")]
         public string Address { get; set; }
+
+        [DataMember(Order = 5, Name = "gender")]
         public string Gender { get; set; }
     }
 }
